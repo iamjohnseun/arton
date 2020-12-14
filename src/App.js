15b17f3ac1faba5442d2f730b8562513c40198b9
John Simeon <iamjohnsimeon@gmail.com>
2020-12-14 22:06:08 +0200

    App Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//VIEWS
+import index from './views/index';
+import quotation from './views/quotation';
+import invoice from './views/invoice';
+import receipt from './views/receipt';
+import report from './views/report';
+import calendar from './views/calendar';
+import setting from './views/setting';
+import profile from './views/profile';
+import error from './views/error';
+
+class App extends Component {
+  state = {
+    redirect: false,
+    link: ''
+  }
+
+  render() {
+    return (
+      <Router>
+          <div className="App">
+            <Switch>
+              <Route path="/" exact component={index}/>
+              <Route path="/quotation" component={quotation} />
+              <Route path="/invoice" component={invoice} />
+              <Route path="/receipt" component={receipt} />
+              <Route path="/profile" component={profile} />
+              <Route path="/reports/:param" component={report} />
+              <Route path="/calendar/:param" component={calendar} />
+              <Route path="/settings/:param" component={setting} />
+              <Route path="/*" component={error} />
+              {/*<Redirect to="/platform"/> */}
+            </Switch>
+          </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
