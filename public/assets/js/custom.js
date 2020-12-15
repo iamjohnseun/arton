22f0a2ce2bde614b7708cfd67b538fd697e38beb
John Simeon <iamjohnsimeon@gmail.com>
2020-12-15 21:53:34 +0200

    Custom JavaScript

---
+++ public/assets/js/custom.js
@@ -0,0 +1,9 @@
+// var elem = document.getElementsByClassName("circular-menu-button");
+// function toggleCircularMenu () {
+//   document.getElementById('circular-menu').classList.toggle('active');
+// }
+// for (var i in elem) {
+//   elem[i].onclick = function() {
+//     toggleCircularMenu();
+//   };
+// }
